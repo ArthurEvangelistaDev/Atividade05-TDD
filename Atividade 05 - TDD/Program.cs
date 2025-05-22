155cc3212dfaf482b4ce06cdfed7f5528f657948
Lucasos8 <Lucas_os8@outlook.com>
2025-05-21 21:18:13 -0300

    Adicionado as funções (parte 2 )

--- Atividade 05 - TDD/Program.cs
+++ Atividade 05 - TDD/Program.cs
@@ -2,7 +2,36 @@
 {
     private static void Main(string[] args)
     {
-        
+        Console.WriteLine("Digite a senha:");
+        String senha = Console.ReadLine();
+
+
+        Console.WriteLine(validaSenha(senha).ToString());
+
+
+    }
+
+    public static String validaSenha(String senha)
+    {
+        String retorno = "";
+        if (senha.Length < 8)
+        {
+            retorno += "A senha deve ter pelo menos 8 caracteres.";
+        }
+        if (senha.Count(char.IsDigit) < 2)
+        {
+            retorno += "A senha deve conter pelo menos 2 dígitos.";
+        }
+        if (!senha.Any(char.IsUpper))
+        {
+            retorno += "A senha deve conter pelo menos uma letra maiuscula.";
+        }
+        if (!senha.Any(c => !char.IsLetterOrDigit(c)))
+        {
+            retorno += "A senha deve conter pelo menos um caractere especial.";
+        }
+
+        return retorno;
     }
 
 }
